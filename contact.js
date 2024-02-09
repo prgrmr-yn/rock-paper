950cef60b4c5170a53397e5f749baffbf0817714
Yatin Nagpal <108704161+prgrmr-yn@users.noreply.github.com>
2024-02-09 17:07:41 +1100

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,3 +1,60 @@
+//contact
+
+const webhookDiscord = 'https://discord.com/api/webhooks/1205391392206094396/BMgS1_yZukA0B_kyHi8JIS2wyw0E33cO5G9jxMjXAMQdyevxc6yFAQo9eowZcRYOrNmi';
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+const content = document.getElementById('content')
+const send = document.getElementById('send')
+const sentMessage = document.getElementById('sent');
+let message ='';
+
+send.addEventListener('click', e=>{
+  e.preventDefault()
+  if (username.value === '') {
+    console.log('not valid');
+    sentMessage.innerHTML = '<strong>Must enter name</strong>'
+  }else if (content.value === '') {
+    sentMessage.innerHTML = '<strong>Must enter a message</strong>'
+  } else{
+    discordIt();
+    console.log(message);
+    username.value = ''
+    email.value = ''
+    content.value = ''
+    setTimeout(() => {
+      sentMessage.style.display = 'none'
+    }, 3000);
+  }
+
+})
+
+function discordIt() {
+  fetch( webhookDiscord, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        "content":`name: ${username.value},
+email: ${email.value},
+message: ${content.value}`,
+        "embeds": null,
+    })
+  })
+   .then(function(res) {
+    if (res.status.toString().startsWith('2')) {
+      message = 'Sent successfully'
+    }else {
+      message = 'Something went wrong, Please try again'
+    }
+    sentMessage.innerHTML = `<strong> ${message}</strong>`
+   })
+  // .then(response => response.json())
+  // .then(response => JSON.stringify(response))
+}
+
+
 //autosend
 
 const webhookUrl = 'https://discord.com/api/webhooks/1205389933133439026/3INtBZGVld9gpoGxgPyBaz2ueMgp4uAOF4W9zwEOJkYpeHH5Vtn93RQllGk2ovFcohvu';
